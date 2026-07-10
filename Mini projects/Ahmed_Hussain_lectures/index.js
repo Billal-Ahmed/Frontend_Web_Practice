b108bfca3acb7aee744c8b38a8482899ef2a1597
Bilal Ahmed <ba4794044@gmail.com>
2026-07-10 16:42:19 +0500

    Just added Ahmed hussain js first lecture

---
+++ Mini projects/Ahmed_Hussain_lectures/index.js
@@ -0,0 +1,35 @@
+// alert("Hello World");
+// console.log("Hello World");
+// document.write("Hello! Ahmed Hussain here. Welcome to my lectures on JavaScript!");
+// alert("Hello World");
+// window.console.log("Hello World");
+// window.document.write("Hello World");
+
+
+// alert ("hello")
+
+
+// console.log("Hello World");
+// console.log("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+
+
+// console.table({"name": "Ahmed Hussain", "age": 25, "city": "Karachi"});
+
+// console.error("This is an error message my own intensionally created for testing purpose.");
+
+
+// console.warn("This is a warning message my own intensionally created for testing purpose.");
+
+
+// console.info("This is an info message my own intensionally created for testing purpose.");
+
+// console.timeEnd("Hello World");
+
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+
